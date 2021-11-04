949594aac08f34c63b85c785e1728ac877cdf098
Mora Ogunro <18519716+MoraOgunro@users.noreply.github.com>
2021-11-04 15:01:27 -0500

    card grid layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="full-screen">
+      <div class="card-container">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Card from './components/Card.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Card
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='sass'>
+$tablet: 600px
+$medium-desktop: 768px
+$desktop: 992px
+$widescreen: 1200px
+body, html
+  margin: 0
+  padding: 0
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+.full-screen
+  width: 100vw
+  height: 100vh
+  overflow-x: hidden
+  overflow-y: hidden
+.card-container
+  display: grid
+  grid-template-columns: repeat(3, 30vw)
+  grid-template-rows: 30vw
+  column-gap: 2vw
+  padding: 0
+  justify-content: space-evenly
+  background-color: gray
+@media screen and (min-width: $widescreen)
+  .card-container
+    width: auto
+    justify-content: space-evenly
+    padding: 0 10vw 0 10vw
+    margin: auto
+    grid-template-columns: repeat(3, max(30vh,300px))
+    grid-template-rows: max(30vh,300px)
+@media screen and (max-width: $tablet)
+  .card-container
+    row-gap: 2em
+    grid-template-columns: 90vw
+    grid-template-rows: repeat(3, 30vh)
+
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='sass'>
+  $tablet: 600px
+  $medium-desktop: 768px
+  $desktop: 992px
+  $widescreen: 1200px
+  $curve: 25px
+
+  .card
+    background-color: lightgreen
+    border-radius: $curve
+</style>
